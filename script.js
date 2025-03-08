592fe56b4744f842fd6bd2dc511008dfcca5d319
EnviroTecSolutions <jayrowley72@hotmail.com>
2025-03-08 16:20:49 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Dropdown menu animation
+document.querySelector('.dropbtn').addEventListener('click', function() {
+    const dropdown = document.querySelector('.dropdown-content');
+    dropdown.classList.toggle('show');
+});
+
+// Optional: Add additional JS functionality if needed in the future
